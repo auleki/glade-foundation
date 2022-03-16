5c0b348b59389aeb679b8c23974234cb8b6e4792
auleki <kodagiwa@gmail.com>
2022-03-16 08:25:24 +0100

    setting up app strucute �‍�

---
+++ README.md
@@ -0,0 +1,2 @@
+# GLADE FOUNDATION 
+This is a landing page to help collect donations to support individuals affected by COVID-19
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Layout from './components/layout/Layout'
+import Landing from './pages/Landing'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Layout>
+      <Landing />
+    </Layout>
+  )
+}
+
+export default App
